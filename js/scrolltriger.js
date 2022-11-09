6a024a8fa52cfd2cd518f0fe9e5259c39e9dc112
DRISS BENADJAL <74878941+drissbenadjal@users.noreply.github.com>
2022-11-09 20:44:06 +0100

    modif

--- js/scrolltriger.js
+++ js/scrolltriger.js
@@ -1,14 +1,28 @@
+/* GSAP TIMELINE */
+const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
+
+tl.to(".text", {
+  y: "0%",
+  duration: 1.5,
+  stagger: 0.25
+});
+
+tl.to(".loading", {
+  y: "-100%",
+  duration: 1
+}); /* , "-=1" */
+
 gsap.registerPlugin(ScrollTrigger);
 
 const images = gsap.utils.toArray("img");
 
 const showDemo = () => {
   document.scrollingElement.scrollTo(0, 0);
-  setTimeout(() => {
-    gsap.to(document.querySelector(".loading"), { autoAlpha: 0 });
-    gsap.to(".loading", { display: "none" });
-    document.body.style.overflowY = "auto";
-  }, 6500);
+  // setTimeout(() => {
+  //   gsap.to(document.querySelector(".loading"), { autoAlpha: 0 });
+  //   gsap.to(".loading", { display: "none" });
+  //   document.body.style.overflowY = "auto";
+  // }, 6500);
 
   gsap.utils.toArray(".scroll-x").forEach((section, index) => {
     const w = section.querySelector(".wrapper");
@@ -47,3 +61,5 @@ const showDemo = () => {
 };
 
 imagesLoaded(images).on("always", showDemo);
+
+
